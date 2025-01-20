61ee55131ff80295af8ad8c48442b96f3d376bc8
Via <ISN.awesome@gmail.com>
2025-01-19 22:16:08 -0900

    added more stuff lol

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# cli-utils
+# cli-utils by viatech
 a simple cli utility program
+
+# functions in the program
+> showpath/sp: prints your current PATH variable
+> fileinfo/fi: shows file info
+> chfi/cf: change file info
+> exit/e: exit the program
--- src/app.cpp
+++ src/app.cpp
@@ -2,17 +2,28 @@
 #include <iostream>
 #include <filesystem>
 #include <string.h>
+#include <map>
 
+// our command files
 #include "./commands/directories.h"
 #include "./commands/files.h"
 #include "./commands/path.h"
+#include "./commands/exit.h"
 
 using namespace std;
 
+// main program that will run
 int main()
 {
+    // all available commands 
     cout << "welcome to the viatech cli utils!\n"; 
     cout << "here, you can do some stuff like: \n";
     cout << "[showpath/sp]: print your path!\n[fileinfo/fi]: print some file info!";
     cout << "[chfi/cf]: change file info\n[exit/e]: exit the program!";
 }
+
+/*
+TODO:
+ - add functionaility to all commands shown in the options
+ - figure out how to use <filesystem>
+*/
